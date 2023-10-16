d97f864bfad554834930b65a3fd8cb128e26d1ef
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-10-16 15:35:29 +0200

    Added Firebase Auth

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvKDAFGntmnNUDk8pOb_tp6OZSSg704rQ",
+  authDomain: "web-app-project-85b05.firebaseapp.com",
+  projectId: "web-app-project-85b05",
+  storageBucket: "web-app-project-85b05.appspot.com",
+  messagingSenderId: "1024657208956",
+  appId: "1:1024657208956:web:488faa6aeaec45ff9ffbf9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(app);
