616e180ba895ed1b5f69f76ca78a4566d595b697
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-10-17 13:27:33 +0200

    check if user exists

--- src/Helperfunctions/CustomSignUp.js
+++ src/Helperfunctions/CustomSignUp.js
@@ -1,26 +1,64 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { FIREBASE_AUTH } from "../../firebase-config";
+import { FIREBASE_AUTH, db } from "../../firebase-config";
 import { firebaseErrorsCodes } from "../../firebaseErrorCodes";
 import { useNavigate } from "react-router-dom";
+import { collection, query, where, getDocs, addDoc, setDoc, doc } from "firebase/firestore";
 
-export function CustomSignUp(form, customSetLoad, navigate) {
+async function insertUsername(signUpUsername, navigate) {
+
+  try {
+    // const docRef = await setDoc(collection(db, "users", user.userName ), user)
+
+    await setDoc(doc(db, "users", signUpUsername), {
+      userName: signUpUsername,
+      inviteCode: signUpUsername,
+    });
+
+    navigate("/home");
+
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+
+export async function CustomSignUp(form, customSetLoad, navigate) {
   // Sets the loading state to true, so user gets feedback
   // afterwards signin in with the passed values
 
-  customSetLoad(true);
+  // customSetLoad(true);
+
+  const signUpUsername = form.current?.signUpUsername.value;
 
   const signUpEmail = form.current?.signUpEmail.value;
   const signUpPassword = form.current?.signUpPassword.value;
+
+  if (!signUpUsername) {
+    return
+  }
+
+  const q = query(collection(db, "users"));
+
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((name) => {
+    // doc.data() is never undefined for query doc snapshots
+
+    if(signUpUsername === name.id) {
+      alert("Hov, to ens...")
+      return true
+    }
+  });
+
 
   try {
     createUserWithEmailAndPassword(FIREBASE_AUTH, signUpEmail, signUpPassword)
       .then((userCredential) => {
         // Signed up
         const user = userCredential.user;
-        alert(user.uid);
         form.current?.reset();
-        navigate("/home");
 
+        insertUsername(signUpUsername, navigate)
         // ...
       })
       .catch((error) => {
