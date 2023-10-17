1c4e9c910d43a76f935ffc4635b5f34ee5d3adb7
Christoffer Rod Greffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-10-17 09:27:02 +0200

    create css and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,31 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { FIREBASE_AUTH } from "../firebase-config";
-import SignInForm from "./Components/SignInForm";
-import SignUpForm from "./Components/SignUpForm";
+import SignUp from "./pages/SignUp";
 import { CustomSignOut } from "./Helperfunctions/CustomSignOut";
 
 function App() {
+  const [user, setUser] = useState("");
 
-  const [user, setUser] = useState('')  
-  
   useEffect(() => {
-    
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
         const uid = user.uid;
-        setUser(uid)
+        setUser(uid);
         // ...
       } else {
-        setUser('')
+        setUser("");
         // User is signed out
         // ...
       }
     });
-  }, [])
+  }, []);
 
   return (
     <>
-      <SignUpForm />
-      <SignInForm />
-
-      <button onClick={CustomSignOut}>Sign out</button>
-
-      <h2>{user}</h2>
-
-      <h1>Hej Christoffer, det virker stadig</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <SignUp />
     </>
   );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUpForm from "../Components/SignUpForm";
+
+function SignUp() {
+  return (
+    <div>
+      {/* <img src={logo} alt="WatchBuddy logo" /> */}
+      <p>Welcome back, please log in</p>
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default SignUp;
