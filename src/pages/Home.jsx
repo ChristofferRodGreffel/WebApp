3912dadbb11927440d442a2d4aaf50385fbc52fa
Christoffer Rod Greffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-10-18 10:45:18 +0200

    work in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { CustomSignOut } from "../Helperfunctions/CustomSignOut";
+import { staticMovies } from "../staticmovies";
 
 const Home = () => {
   const handleSignOut = () => {
-    CustomSignOut()
-  }
-  
+    CustomSignOut();
+  };
+
   return (
     <>
       <div>Home</div>
       <button onClick={handleSignOut}>Sign out</button>
+      <div>
+        {staticMovies.movies.map((movie, key) => {
+          return (
+            <img
+              style={{ width: "140px", height: "200px", objectFit: "cover" }}
+              src={movie.poster_image}
+              key={key}
+              alt="movie poster"
+            />
+          );
+        })}
+      </div>
     </>
-
-  )
+  );
 };
 
 export default Home;
