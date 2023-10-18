bdc13f35fc7cad1123e6af64f5beb552efe94493
nra95 <noreskelund@gmail.com>
2023-10-18 11:01:32 +0200

    MovieCard

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 function MovieCard(props) {
   return (
-    <div>
-      <img src={props.url} alt={`${props.title} Movie poster`} />
-
+    <div className="moviecardWrapper">
       <div>
-        <i className="fa-solid fa-star"></i>
+        <img src={props.url} alt={`${props.title} Movie poster`} />
+
+        <div className="moviecardRatingContainer">
+          <i className="fa-solid fa-star"></i>
+          <p>{props.rating}</p>
+        </div>
+
+        <div className="moviecardAdd">
+          <i className={props.icon}></i>
+        </div>
       </div>
+
+      <p>{props.title}</p>
     </div>
   );
 }
