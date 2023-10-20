96161fcb425e805288f5eb2559e24303a431de1f
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-10-20 09:48:08 +0200

    movie overview img

--- src/pages/SingleMovieOverview.jsx
+++ src/pages/SingleMovieOverview.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
+import { staticMovies } from '../staticmovies';
 
 function SingleMovieOverview() {
-    
+
     const { imdbid } = useParams();
 
 
     return (
-        <div style={{fontSize: 128}}>{imdbid}</div>
+        <>
+            {staticMovies.movies.map((listItem) => {
+                if (listItem.imdb_id === imdbid) {
+                    return (
+                        <div key={listItem.imdb_id}>
+                            <img style={{width: '20%'}} src={listItem.poster_image} alt={`${listItem.title} poster`} />
+                        </div>
+                    );
+                }
+            })}
+
+            <div style={{ fontSize: 128 }}>{imdbid}</div>
+        </>
     )
 }
 
