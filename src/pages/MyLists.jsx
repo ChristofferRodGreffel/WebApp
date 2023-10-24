0a0eaa7301120ec39cbbf17c03581c15427648ca
HGrunberg <113166809+HGrunberg@users.noreply.github.com>
2023-10-24 13:17:39 +0200

    my list work

--- src/pages/MyLists.jsx
+++ src/pages/MyLists.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddList from "../Components/AddListBtn";
 import CreateNewList from "../Components/CreateNewList";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../firebase-config";
+import HorizontalScroller from "../Components/HorizontalScroller";
+import { staticMovies } from "../staticmovies";
+import MovieCard from "../Components/MovieCard";
 
 const MyLists = () => {
-  const [myLists, setMyLists] = useState();
+  const [myLists, setMyLists] = useState([]);
   const handleAddList = (e) => {
     e.stopPropagation();
     const popup = document.querySelector(".addToList");
     popup.style.display = "flex";
   };
+
+  useEffect(() => {
+    const getAllLists = async () => {
+      setMyLists([])
+      const querySnapshot = await getDocs(collection(db, "lists"));
+      querySnapshot.forEach((list) => {
+
+        const listData = list.data()
+
+        setMyLists((myList) => [...myList, listData])
+      });
+    }
+    getAllLists()
+  }, [])
 
   return (
     <div className="my-lists">
@@ -17,7 +36,33 @@ const MyLists = () => {
         <h1>My lists</h1>
         <AddList function={handleAddList} />
       </div>
-      <div className="all-lists">{myLists ? <p></p> : <p>You have not made any lists...</p>}</div>
+      <div className="all-lists">{myLists.length != 0 ?
+        <>
+          {myLists.map((list, key) => {
+            return (
+              <HorizontalScroller
+                scrollerTitle={list.listName}
+                content={list.movies.length != 0 ?
+                  <>
+                    {list.movies?.map((movie, key) => {
+                      return <MovieCard key={key} id={movie.imdb_id} title={movie.title} url={movie.poster_image} rating={movie.rating.toPrecision(2)} icon={"fa-solid fa-plus"} />;
+                    })}
+                  </>
+                  :
+                  <>
+                    <p>Add a movie to the list you dumbass</p>
+                  </>
+                }
+              />
+            )
+          })}
+        </>
+        :
+        <>
+          <p>You have not made any lists...</p>
+        </>
+      }
+      </div>
     </div>
   );
 };
